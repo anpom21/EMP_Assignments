0842efe6915fea1f30a975e4ef5ccad7ae63de8a
markus simonsen <markus.k.simonsen@gmail.com>
2023-03-02 10:57:09 +0100

    'LEDFiles'

---
+++ Assignement_2_final/LED.c
@@ -0,0 +1,64 @@
+/*****************************************************************************
+* University of Southern Denmark
+* Embedded C Programming (ECP)
+*
+* MODULENAME.: LED.C
+*
+* PROJECT....: Assignment 2 - Traffic light
+*
+* DESCRIPTION: Setup GPIO primarily for LEDS and testing
+*
+* Change Log:
+******************************************************************************
+* Date  Id  Change
+* 02-02-2023
+* --------------------
+* 02-03-2023  MoH   Module created.
+*
+*****************************************************************************/
+/***************************** Include files *******************************/
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+#include "emp_type.h"
+#include "gpio.h"
+/*****************************    Defines    *******************************/
+//---colors for LED-----------
+/*#define LED_GREEN 4
+#define LED_BLUE 2
+#define LED_CYAN 6
+#define LED_RED 1
+#define LED_YELLOW 5
+#define LED_MAGENTA 3
+#define LED_WHITE 7
+*/
+#define START_BIT_LED   1
+
+/*****************************   Constants   *******************************/
+
+/*****************************   Variables   *******************************/
+
+/*****************************   Functions   *******************************/
+
+void LED_EMP(INT8U LED_color)
+/*****************************************************************************
+*   Input    : Desired color as seen under defines
+*   Output   : Sets the desired color on the EMP board
+*   Function : --
+******************************************************************************/
+{
+    GPIO_PORTF_DATA_R &= ~(0b111 << START_BIT_LED); //Clear the LEDs
+    GPIO_PORTF_DATA_R |= (LED_color) << START_BIT_LED; //Set the desired color
+}
+
+void LED_EMP_expansion(INT8U LED_color)
+/*****************************************************************************
+*   Input    : Desired color as seen under defines
+*   Output   : Sets the desired color on the EMP expansion board
+*   Function : --
+******************************************************************************/
+{
+    LED_color = ~(LED_color);
+    GPIO_PORTF_DATA_R &= ~(0b111 << START_BIT_LED); //Clear the LEDs
+    GPIO_PORTF_DATA_R |= (LED_color) << START_BIT_LED; //Set the desired color
+}
+/****************************** End Of Module *******************************/
